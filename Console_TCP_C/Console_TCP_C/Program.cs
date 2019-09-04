ca0b5c291ef55aad197c02e4f6108b82712b025b
jump8779 <jump154159@gmail.com>
2019-09-05 00:00:38 +0900

    TCP 통신 클라이언트 코드 작성

--- Console_TCP_C/Console_TCP_C/Program.cs
+++ Console_TCP_C/Console_TCP_C/Program.cs
@@ -4,13 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Net;
+using System.Net.Sockets;
+
 namespace Console_TCP_C
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            try
+            {
+                TcpClient msgTcpc = new TcpClient();
+                msgTcpc.Connect("localhost", 8000);
+
+                NetworkStream cNts = msgTcpc.GetStream();
+                Byte[] byteReceive = new Byte[128];
+                int i = cNts.Read(byteReceive, 0, 128);
+                String strReceive = Encoding.Default.GetString(byteReceive);
+                Console.WriteLine("{0}바이트를 받았습니다.", i);
+                Console.WriteLine("현재 날짜 및 시간 : {0}", strReceive);
+
+                cNts.Close();
+                msgTcpc.Close();
+            }
+            catch(SocketException ex)
+            {
+                if(ex.ErrorCode == 10061)
+                {
+                    Console.WriteLine("서버에 연결할 수 없습니다.");
+                }
+            }
+
         }
     }
 }
