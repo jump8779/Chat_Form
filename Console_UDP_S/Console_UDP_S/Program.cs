84fe8407f25a8969f54ffde3bcab8c177b6293a8
jump8779 <jump154159@gmail.com>
2019-09-05 01:33:22 +0900

    Console_UDP 서버 코드 작성

--- Console_UDP_S/Console_UDP_S/Program.cs
+++ Console_UDP_S/Console_UDP_S/Program.cs
@@ -4,12 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Net;
+using System.Net.Sockets;
+
 namespace Console_UDP_S
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int port = 8000;
+            try
+            {
+                UdpClient udpS = new UdpClient(port);
+                Console.WriteLine("연결할 클라이언트를 기다리고 있습니다.");
+                Console.WriteLine("끝내려면 <Ctrl+C>를 누르십시오.");
+
+                while (true)
+                {
+                    IPAddress ipAd = IPAddress.Any;
+                    IPEndPoint recPoint = new IPEndPoint(ipAd, port);
+                    Byte[] byteRec = udpS.Receive(ref recPoint);
+                    String strRec = Encoding.Default.GetString(byteRec);
+                    Console.WriteLine("[수신]:" + strRec);
+                    String strSend = DateTime.Now.ToString();
+                    Byte[] byteSend = Encoding.Default.GetBytes(strSend);
+                    udpS.Send(byteSend, byteSend.Length, recPoint);
+                }
+            }
+            catch(SocketException ex)
+            {
+                if(ex.ErrorCode == 10048)
+                {
+                    Console.WriteLine("이 포트에 연결하지 못했습니다. \n" +
+                        "다른 서버가 이 포트를 사용 중 입니다.");
+                }
+            }
         }
     }
 }
