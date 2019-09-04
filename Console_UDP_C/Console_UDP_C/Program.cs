1b90f0d6018031b22efbf1528509164104986150
jump8779 <jump154159@gmail.com>
2019-09-05 02:35:00 +0900

    Console_UDP 클라이언트 코드 작성

--- Console_UDP_C/Console_UDP_C/Program.cs
+++ Console_UDP_C/Console_UDP_C/Program.cs
@@ -4,12 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Net;
+using System.Net.Sockets;
+
 namespace Console_UDP_C
 {
     class Program
     {
         static void Main(string[] args)
         {
+            try
+            {
+                UdpClient udpC = new UdpClient(8001);
+                Boolean bTag = true;
+
+                while (bTag)
+                {
+                    IPAddress ipAd = IPAddress.Any;
+                    String strSend = "서버의 시간";
+                    Byte[] byteSend = Encoding.Default.GetBytes(strSend);
+                    udpC.Send(byteSend, byteSend.Length, "localhost", 8000);
+                    IPEndPoint recPoint = new IPEndPoint(ipAd, 8000);
+                    Byte[] byteRec = udpC.Receive(ref recPoint);
+                    String strRec = Encoding.Default.GetString(byteRec);
+                    Console.WriteLine("현재 날짜 및 시간:{0}", strRec);
+
+                    udpC.Close();
+                    bTag = false;
+                }
+            }
+            catch(SocketException ex)
+            {
+                if(ex.ErrorCode == 10048)
+                {
+                    Console.WriteLine("이 포트에 연결하지 못했습니다. \n" +
+                        "다른 서버가 이 포트를 사용 중 입니다.");
+                }
+            }
         }
     }
 }
