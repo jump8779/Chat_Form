a21ad12994d7b65ce3609987a6bb70cc44f04c7b
jump8779 <jump154159@gmail.com>
2019-09-04 22:58:05 +0900

    TCP 통신 서버코드 작성

--- Console_TCP_S/Console_TCP_S/Program.cs
+++ Console_TCP_S/Console_TCP_S/Program.cs
@@ -4,12 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Net;
+using System.Net.Sockets;
+
 namespace Console_TCP_S
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int port = 8000;
+            try
+            {
+                TcpListener msgListen = new TcpListener(port);
+                msgListen.Start();
+
+                Console.WriteLine("연결할 클라이언트를 기다리고 있습니다.");
+                Console.WriteLine("끝내려면 <Ctrl+C>를 누르십시오.");
+
+                while(true)
+                {
+                    Socket s = msgListen.AcceptSocket();
+                    NetworkStream msgNts = new NetworkStream(s);
+                    String sMsg = DateTime.Now.ToString();
+                    Byte[] sByte = Encoding.Default.GetBytes(sMsg);
+                    msgNts.Write(sByte, 0, sByte.Length);
+                    msgNts.Flush();
+
+                    msgNts.Close();
+                    s.Close();
+                }
+            }
+            catch(SocketException ex)
+            {
+                if(ex.ErrorCode == 10048)
+                {
+                    Console.WriteLine("이 포트에 연결하지 못했습니다. \n" +
+                        "다른 서버가 이 포트를 사용 중입니다.");
+                }
+            }
         }
     }
 }
