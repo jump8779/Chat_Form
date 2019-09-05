99a6ee0122a82f86f6526544b6f5d46328727a4d
jump8779 <jump154159@gmail.com>
2019-09-05 10:16:31 +0900

    Console_UDP 서버, 클라이언트 주석 추가

--- Console_UDP_C/Console_UDP_C/Program.cs
+++ Console_UDP_C/Console_UDP_C/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//네임스페이스 지정
 using System.Net;
 using System.Net.Sockets;
 
@@ -23,12 +24,17 @@ namespace Console_UDP_C
                     IPAddress ipAd = IPAddress.Any;
                     String strSend = "서버의 시간";
                     Byte[] byteSend = Encoding.Default.GetBytes(strSend);
+                    //데이터 그램 전송
+                    //TCP 프로토콜과는 달리 Connect() 메서드의 호출 없이
+                    //목적지를 정한 뒤 바로 전송
                     udpC.Send(byteSend, byteSend.Length, "localhost", 8000);
                     IPEndPoint recPoint = new IPEndPoint(ipAd, 8000);
+                    //데이터 수신
                     Byte[] byteRec = udpC.Receive(ref recPoint);
                     String strRec = Encoding.Default.GetString(byteRec);
                     Console.WriteLine("현재 날짜 및 시간:{0}", strRec);
 
+                    //연결 종료
                     udpC.Close();
                     bTag = false;
                 }
--- Console_UDP_S/Console_UDP_S/Program.cs
+++ Console_UDP_S/Console_UDP_S/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//네임스페이스 지정
 using System.Net;
 using System.Net.Sockets;
 
@@ -16,19 +17,25 @@ namespace Console_UDP_S
             int port = 8000;
             try
             {
+                //서버 오픈
                 UdpClient udpS = new UdpClient(port);
                 Console.WriteLine("연결할 클라이언트를 기다리고 있습니다.");
                 Console.WriteLine("끝내려면 <Ctrl+C>를 누르십시오.");
 
                 while (true)
                 {
+                    //IPAddress.Any = 0.0.0.0
+                    //로컬 머신의 모든 IP에 대해 Listening 할 경우 사용
                     IPAddress ipAd = IPAddress.Any;
                     IPEndPoint recPoint = new IPEndPoint(ipAd, port);
+
+                    //블록상태에 있다가 데이터 그램 수신
                     Byte[] byteRec = udpS.Receive(ref recPoint);
                     String strRec = Encoding.Default.GetString(byteRec);
                     Console.WriteLine("[수신]:" + strRec);
                     String strSend = DateTime.Now.ToString();
                     Byte[] byteSend = Encoding.Default.GetBytes(strSend);
+                    //데이터 송신
                     udpS.Send(byteSend, byteSend.Length, recPoint);
                 }
             }
